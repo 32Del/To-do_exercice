121f17ca486a392780498c5172bdae1936f8e8b8
Neto <vng150395@gmail.com>
2018-03-08 08:45:56 +0100

    Finis

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿/*****************************************************************
+**																**
+**		Nom		:Neto Gonçalves									**
+**		Prenom	:Victor											**
+**		Date  	:18 Octobre 2013								**
+**		Classe	:I.IN_P3A										**
+**																**
+*****************************************************************/
+
+var varAddHere="";
+
+function add() {
+	document.getElementById('addhere').innerHTML='';
+	varAddHere += "<li>" + document.getElementById('task').value + "</li>";
+	document.getElementById('addhere').innerHTML=varAddHere;
+}
